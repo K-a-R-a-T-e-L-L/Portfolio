4b23bbc3614a8bf378fac185ab6f610f57f49119
K-a-R-a-T-e-L-L <kirillcuhorukov6@gmail.com>
2024-10-02 17:54:55 +0700

    Implementation of obtaining information about projects from the server and displaying them

--- client/src/components/Pages/Projects/Projects.jsx
+++ client/src/components/Pages/Projects/Projects.jsx
@@ -1,13 +1,14 @@
 import { useTranslation } from 'react-i18next';
 import style from './styles.module.scss';
 import { useLocalStorage } from '../../../hooks/useLocalStorage/useLocalStorage';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import LanguageChangeButtons from '../../Reused/LanguageChangeButtons/LanguageChangeButtons';
 import BackButton from '../../Reused/BackButton/BackButton';
+import axios from 'axios';
 
 const Projects = () => {
 
-
+    const [DataProjectsArray, setDataProjectsArray] = useState([]);
     const { i18n } = useTranslation();
     const [DisplayButton, setDisplayButton] = useLocalStorage('page-language', 'ru');
 
@@ -20,54 +21,32 @@ const Projects = () => {
         changeLanguage(DisplayButton);
     }, []);
 
+    useEffect(() => {
+        axios.get('http://localhost:4000/projects')
+            .then((res) => {
+                setDataProjectsArray(res.data);
+            })
+            .catch((err) => { console.log(`Error receiving data from the server: ${err}`) });
+    }, []);
+
     return (
         <>
             <div className={style.projects}>
                 <LanguageChangeButtons DisplayButton={DisplayButton} setDisplayButton={setDisplayButton} left={'30px'} />
                 <BackButton pathBack={'/'} />
                 <div className={style.projects__box}>
-                    <div className={style.box__project}>
-                        <div className={style.project__img}></div>
-                        <div className={style.project__info}>
-                            <h5>NAME</h5>
-                            <h6>info</h6>
-                        </div>
-                    </div>
-                    <div className={style.box__project}>
-                        <div className={style.project__img}></div>
-                        <div className={style.project__info}>
-                            <h5>NAME</h5>
-                            <h6>info</h6>
-                        </div>
-                    </div>
-                    <div className={style.box__project}>
-                        <div className={style.project__img}></div>
-                        <div className={style.project__info}>
-                            <h5>NAME</h5>
-                            <h6>info</h6>
-                        </div>
-                    </div>
-                    <div className={style.box__project}>
-                        <div className={style.project__img}></div>
-                        <div className={style.project__info}>
-                            <h5>NAME</h5>
-                            <h6>info</h6>
-                        </div>
-                    </div>
-                    <div className={style.box__project}>
-                        <div className={style.project__img}></div>
-                        <div className={style.project__info}>
-                            <h5>NAME</h5>
-                            <h6>info</h6>
-                        </div>
-                    </div>
-                    <div className={style.box__project}>
-                        <div className={style.project__img}></div>
-                        <div className={style.project__info}>
-                            <h5>NAME</h5>
-                            <h6>info</h6>
-                        </div>
-                    </div>
+                    {DataProjectsArray.map((el, i) => {
+                        return (
+                            <div className={style.box__project} key={i}>
+                                <div className={style.project__img}></div>
+                                <div className={style.project__info}>
+                                    <h5>{el.name}</h5>
+                                    <h6>{el.info}</h6>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <button className={style.box__adding_button}>+</button>
                 </div>
             </div>
         </>
--- server/server.js
+++ server/server.js
@@ -8,12 +8,55 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.db');
 
 // db.run(`CREATE TABLE projects_data
-    // (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, link TEXT NOT NULL, img BLOB NOT NULL, info TEXT NOT NULL)`);
+//     (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, link TEXT NOT NULL, img BLOB NOT NULL, info TEXT NOT NULL)`);
 // db.run('DELETE FROM projects_data');
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/projects', async () => { });
+app.get('/adding', async (req, res) => {
+    const data = req.body;
+    try {
+        db.run('INSERT INTO projects_data (name, link, img, info) VALUES(?, ?, ?, ?);', [data.name, data.link, data.img, data.info], function (err) {
+            if (err) {
+                console.log(`Error writing data: ${err}`);
+                res.send(`Error writing data: ${err}`);
+            };
+        });
+        db.all('SELECT * FROM projects_data;', (err, rows) => {
+            if (err) {
+                console.log(`Error in data extraction: ${err}`);
+                res.send(`Error in data extraction: ${err}`);
+            }
+            else {
+                res.send(rows);
+                console.log(rows);
+            }
+        })
+    }
+    catch (err) {
+        console.log(`Server error when adding data: ${err}`);
+        res.send(`Server error when adding data: ${err}`);
+    };
+});
+
+app.get('/projects', async (req, res) => {
+    try {
+        db.all('SELECT * FROM projects_data;', (err, rows) => {
+            if (err) {
+                console.log(`Error when receiving project data: ${err}`);
+                res.send(`Error when receiving project data: ${err}`);
+            }
+            else {
+                res.send(rows);
+                console.table(rows);
+            }
+        })
+    }
+    catch (err) {
+        console.log(`Server error while receiving data: ${err}`);
+        res.send(`Server error while receiving data: ${err}`);
+    };
+});
 
 app.listen(port, () => console.log('Server started'));
